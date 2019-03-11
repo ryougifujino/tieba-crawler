a575bcca05c16185abfcdefc2889e2faaece8e48
ryougifujino <ryougifujino@gmail.com>
2019-03-11 13:41:38 +0800

    Optimize network connections

--- data/tb-apis.js
+++ data/tb-apis.js
@@ -1,8 +1,15 @@
 const {JSDOM} = require('jsdom');
-const http = require('../util/http-promise.js');
+const request = require('../util/request-promise.js');
+
+let barProtocols = {};
 
 const getThreadList = async (barName, page) => {
-    const body = await http.get(`http://tieba.baidu.com/f?kw=${barName}&ie=utf-8&pn=${page}`);
+    let protocol = barProtocols[barName];
+    if (!protocol) {
+        protocol = await request.getProtocol(`://tieba.baidu.com/f?kw=${barName}&ie=utf-8`);
+        barProtocols[barName] = protocol;
+    }
+    const body = await request.get(protocol, `://tieba.baidu.com/f?kw=${barName}&ie=utf-8&pn=${page}`);
     let {window: {document}} = new JSDOM(body);
     let threads = [...document.querySelectorAll('#content_leftList .j_thread_list.clearfix')];
 
---
+++ util/request-promise.js
@@ -0,0 +1,44 @@
+const http = require('http');
+const https = require('https');
+
+const PROTOCOL_HTTP = "http";
+const PROTOCOL_HTTPS = "https";
+
+const REQUEST = {http, https};
+
+
+function getProtocol(url) {
+    url = PROTOCOL_HTTP + encodeURI(url);
+    return new Promise((resolve, reject) => {
+        http.get(url, response => {
+            let {statusCode} = response;
+            switch (statusCode) {
+                case 200:
+                    resolve(PROTOCOL_HTTP);
+                    break;
+                case 301:
+                    resolve(PROTOCOL_HTTPS);
+                    break;
+                default:
+                    reject(new Error(`Request failed, status code: ${statusCode}`));
+            }
+        });
+    });
+}
+
+function get(protocol, url) {
+    url = protocol + encodeURI(url);
+    return new Promise((resolve, reject) => {
+        REQUEST[protocol]['get'](url, response => {
+            let body = [];
+            response.on('data', chunk => body.push(chunk));
+            response.on('end', () => resolve(Buffer.concat(body).toString()))
+                .on('error', err => reject(err));
+        });
+    });
+}
+
+module.exports = {
+    get,
+    getProtocol
+};
